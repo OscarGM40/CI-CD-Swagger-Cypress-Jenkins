1684dc054a8189da8ab575748fdbc04f6cbdbd3b
Oscar <oscargm28@hotmail.com>
2022-01-17 23:46:12 +0100

    improving with Swagger

--- sample03SwaggerNodeExpressInDepth/index.js
+++ sample03SwaggerNodeExpressInDepth/index.js
@@ -10,22 +10,69 @@ app.use(express.json());
 app.use(
   "/api-docs",
   swaggerUI.serve, //.serve para que sirva la UI
-  swaggerUI.setup(swaggerJsDocs));
+  swaggerUI.setup(swaggerJsDocs)
+);
 
-app.get("/string",(req, res) => {
+app.get("/string", (req, res) => {
   res.send("This is a String");
-})
+});
 
-app.get("/user",(req, res) => {
+app.get("/user", (req, res) => {
   res.send({
-    id:1,
+    id: 1,
     name: "John",
     lastName: "Doe",
-    age: 30
+    age: 30,
   });
-})
+});
+
+app.get("/users", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      name: "John",
+      lastName: "Doe",
+      age: 30,
+    },
+    {
+      id: 2,
+      name: "Jane",
+      lastName: "Doe",
+      age: 33,
+    },
+    {
+      id: 1,
+      name: "Amee",
+      lastName: "Farenheit",
+      age: 22,
+    },
+  ]);
+});
+
+app.get("/users/:userId", (req, res) => {
+  res.send({
+      id: 1,
+      name: "John",
+      lastName: "Doe",
+      age: 30,
+    });
+});
+
+app.post("/create", (req, res) => {
+  const {name,lastName,age} = req.body;
+  const user = { name,lastName,age }
+
+  res.send({
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      age: user.age,
+    });
+});
 
 app.listen(3000, () => {
   // console.clear()
-  console.log("Server running on port 3000.Swagger UI on http://localhost:3000/api-docs");
+  console.log(
+    "Server running on port 3000.Swagger UI on http://localhost:3000/api-docs"
+  );
 });
